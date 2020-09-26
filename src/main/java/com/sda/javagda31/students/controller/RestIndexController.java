4e7c537224058257bca1f3d23d41cd57e422e1cf
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-09-26 11:31:13 +0200

    addTwoValues

--- src/main/java/com/sda/javagda31/students/controller/RestIndexController.java
+++ src/main/java/com/sda/javagda31/students/controller/RestIndexController.java
@@ -19,4 +19,11 @@ public class RestIndexController {
     public ApiResponse welcome(@RequestParam(name = "imieTwe") String imie) {
         return new ApiResponse("Welcome" + imie + "!");
     }
+    //1.  Stwórz endpoint pod adresem /add który pobiera dwa parametry i dodaje je do siebie, po czym zwraca wynik nie opakowany w APIResponse
+    //    http://localhost:8080/api/add?liczba1=100&liczba2=50
+    @GetMapping("/add")
+    public int addTwoValues(@RequestParam(name= "liczba1") int liczba1, @RequestParam(name="liczba2") int liczba2){
+        return liczba1+liczba2;
+    }
+
 }
