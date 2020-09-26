1ac2e840023beda60684ae73d2e84df519eee420
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-09-26 14:40:04 +0200

    student_form

--- src/main/java/com/sda/javagda31/students/controller/StudentController.java
+++ src/main/java/com/sda/javagda31/students/controller/StudentController.java
@@ -1,9 +1,28 @@
 package com.sda.javagda31.students.controller;
 
+import com.sda.javagda31.students.model.Student;
+import com.sda.javagda31.students.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    @GetMapping("/add")
+    public String getForm(Model model ){
+        model.addAttribute("newStudent", new Student());
+        return "student_form";
+    }
+    @PostMapping("/add")
+    public String submitForm(Student student){
+
+    }
 }
--- src/main/java/com/sda/javagda31/students/service/StudentService.java
+++ src/main/java/com/sda/javagda31/students/service/StudentService.java
@@ -1,7 +1,15 @@
 package com.sda.javagda31.students.service;
 
+import com.sda.javagda31.students.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+// Dependency Injection mechanizm
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired //inject
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 }
