bb7490c495ce38c17ea77932423b29569d4a1373
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-09-26 10:27:08 +0200

    RestIndexController

--- src/main/java/com/sda/javagda31/students/controller/RestIndexController.java
+++ src/main/java/com/sda/javagda31/students/controller/RestIndexController.java
@@ -1,8 +1,15 @@
 package com.sda.javagda31.students.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class RestIndexController {
 
+    @GetMapping("/")
+    public String index(){
+        return "Hello!";
+    }
 }
